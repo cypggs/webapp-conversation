4b42e9cdea9d8a97d48b6496c38e3de0ab338641
cypggs <qcypggs@qq.com>
2024-12-17 22:05:46 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,8 +3,8 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Llama-3.2-90b',
-  description: 'Llama-3.2-90b',
+  title: 'Llama-3.3-70b',
+  description: 'Llama-3.3-70b update 2024-12-17',
   copyright: '',
   privacy_policy: '',
   default_language: 'en',
